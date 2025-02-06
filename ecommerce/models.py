c6310dde1bb130da19f16a0adc94018d654ed090
Gustavo Emanuel <gustavoe.dev@gmail.com>
2025-02-06 20:14:47 -0300

    feat: add models for Fornecedor, Categoria, and Produto

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Fornecedor(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nome
+
+class Categoria(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
+
+class Produto(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField(blank=True, null=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    quantidade = models.IntegerField()
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    fornecedor = models.ForeignKey(Fornecedor, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
