fb876e401c64e08c730835eeed376762ae3770d6
Gustavo Emanuel <gustavoe.dev@gmail.com>
2025-02-06 21:52:24 -0300

    feat: register Fornecedor, Produto, and Categoria models in admin site

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Fornecedor)
+admin.site.register(Produto)
+admin.site.register(Categoria)
